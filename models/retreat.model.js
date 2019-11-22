7d9304ce88739bd329765af993bbf82f337c057b
DaviCrisostomo <davicrisostomo@gmail.com>
2019-11-22 20:05:25 +0000

    model functions

---
+++ models/retreat.model.js
@@ -0,0 +1,17 @@
+const filename = '../data/retreats.json'
+let retreats = require(filename)
+const helper = require('../helpers/helper')
+
+function getRetreats(){}
+function getRetreat(id){}
+function insertRetreat(newRetreat){}
+function updateRetreat(id, newRetreat){}
+function deleteRetreat(id){}
+
+module.exports={
+    getRetreat,
+    getRetreat,
+    insertRetreat,
+    updateRetreat,
+    deleteRetreat
+}
