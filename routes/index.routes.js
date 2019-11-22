fe73ea96e2980dd794873d3e32ce5463df5c99b2
DaviCrisostomo <davicrisostomo@gmail.com>
2019-11-22 20:45:51 +0000

    routes

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,3 +1,4 @@
 const express = require('express')
 const router = express.Router()
-module.exports = routerrouter.use('/api/v1/retreats', require('./retreat.routes'))
+module.exports = router
+router.use('/api/v1/retreats', require('./retreat.routes'))
