7ae583fe584a4bbb5ab96a07720afa4f2bf3d9da
DaviCrisostomo <davicrisostomo@gmail.com>
2019-11-22 20:48:42 +0000

    index

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 // Import packages
 const express = require('express')
+const router = express.Router()
+const retreat = require('../models/retreat.model')
+const m = require('../helpers/middlewares')
+// Routes
+
 const morgan = require('morgan')// App
 const app = express()// Morgan
+
 app.use(morgan('tiny'))// First route
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(require('./routes/index.routes'))
+/*
 app.get('/', (req, res) => {
     res.json({ message: 'Juicy Leine' })
-})// Starting server
+})*/
+// Starting server
 app.listen('1337')
