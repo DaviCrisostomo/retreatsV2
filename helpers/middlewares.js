f30f64f603b18fce729843c2df22b2a7ab192162
DaviCrisostomo <davicrisostomo@gmail.com>
2019-11-24 12:28:27 +0000

    middlewares functions refactored

--- helpers/middlewares.js
+++ helpers/middlewares.js
@@ -9,40 +9,57 @@ function integerChecker(req, res, next) {
     }
 }
 
-function dateValidation(req, res, next) {
-    const date = req.params.date
-    const arrival = DateTime.fromISO(date.arrival)//"2017-05-15"=> May 15, 2017 at midnight
-    const departure = DateTime.fromISO(date.departure)
+const dateValidation = (date) => {
+   
+    const checkDate = DateTime.fromISO(date)//"2017-05-15"=> May 15, 2017 at midnight
 
-    if (arrival.isValid && departure.isValid) {
-        next()
-    }
-    else {
-        res.status(400).json({ message: 'Date format is not valid' })
-    }
+    return checkDate.isValid
+  
+}
+
+const nameValidation = (name) => {
+
+    const reg = /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/u
+
+    return reg.test(name)
 
 }
 
-function validateEmail(req, res, next) {
-    var reg = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    const email = req.params.email
-    if (reg.test(email)) {
-        next()
-    } else {
-        res.status(400).json({ message: 'Invalid format for email' })
-    }
 
+const validateEmail = (email) => {
+    
+    const reg = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+    return reg.test(email)
+
+}
+
+const contactsValidation = (array) => {
+
+    for (i = 0; i < array.length; i++) {
+        if (!nameValidation(array[i].name) || !validateEmail(array[i].email)) {
+            return false
+        }
+    }
+    return true
 }
 
 function checkRequiredFields(req, res, next) {
 
-    const {date, location, contacts} = req.body
-   
-    if (date.arrival && date.departure && location && contacts) {
-      
+    const { date, location, contacts } = req.body
+
+    if (!location.country || !location.city || !location.address) {
+        res.status(400).json({ message: 'Location fields can\'t be null' })
+    }
+    else if (!dateValidation(date.arrival) && !dateValidation(date.departure)) {
+        res.status(400).json({ message: 'Date format is not valid' })
+    }
+
+    else if (!contactsValidation(contacts)) {
+        res.status(400).json({ message: 'One or more contact fields missing information or in wrong format' })
+    }
+    else {
         next()
-    } else {
-        res.status(400).json({ message: 'Required fields can\'t be null' })
     }
 
 }
@@ -50,6 +67,4 @@ function checkRequiredFields(req, res, next) {
 module.exports = {
     integerChecker,
     checkRequiredFields,
-    validateEmail,
-    dateValidation
 }
