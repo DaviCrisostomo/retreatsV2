ae20917f06bc436d8f2ecc716b349384ccedf625
DaviCrisostomo <davicrisostomo@gmail.com>
2019-12-08 16:42:34 +0000

    view-crud separated

--- front_end/view.js
+++ front_end/view.js
@@ -1,6 +1,48 @@
 
 var div = document.getElementById("cardDiv")
 
+function retreatsGrid(retreatArray) {
+    
+    for (let i = 0; i < retreatArray.length; i++) {
+      div.innerHTML += `
+                      <div class="card col-4 m-3 mx-auto" onclick=getRetreat(${retreatArray[i].id})>
+                          <img class="card-img-top" src="img/_meditation.png"/>
+                          <div class="card-body">
+                              <h5 class="card-title">${retreatArray[i].title }</h5>
+                              <p class="card-info">${retreatArray[i].date}</p>
+                          </div>
+                      </div>
+                      `                  
+        }
+     
+  }
+
+  function showRetreat(retreat) {
+ 
+        div.innerHTML = `
+        <div class="card col-5 m-1 mx-auto">
+       <img class="card-img-top" src="img/_meditation.png" />
+       <div class="card-body" >
+        <h3 class="card-title" name="jsonField" id="title" contentEditable = "true">${retreat.title}</h3>
+        <h6 class="card-title" name="jsonField" id="mail" contentEditable = "true">${retreat.contactMail}</h3>
+        <h6 class="card-info" name="jsonField" id="date" contentEditable = "true">${retreat.date}<class contentEditable = "false">&nbsp;(</class><name="jsonField" id="duration" contentEditable = "true"><i>${retreat.duration}</><class contentEditable = "false">&nbsp;days)</i></></h6>
+        <h5 name="jsonField" id="description" contentEditable = "true">${retreat.description}</h5>
+       
+      <!--  <button class="beditor" id="editButton" onclick="setEditable()"  style="float:right;">Edit</button>--!>
+       
+        <div id="room-table"></div>     
+        <table class="table">
+        <button class="button btn-lg" id="calculate-button">Total</button>
+        <input type="text " size="8"name="display" id="display" value="€0"/>
+        <button class="button btn-lg float-none" id="create-button">Insert</button>
+        <button class="button btn-lg float-none" id="exclude-button">Remove</button>
+        <button class="button btn-lg float-none" id="confirm-button">Update</button>
+        <button class="button btn-lg float-right" id="delete-button">Delete</button>
+        </div>
+       `
+        fafafunction(retreat);
+  }
+
 function newRetreatForm(){
 
  div.innerHTML = ` 
@@ -20,9 +62,16 @@ function newRetreatForm(){
 </div>
 </div>`
 
-
-
 }
+
+function fafafunction(retreat) {
+    createTable(retreat);
+  }
+
+function insertAbout() {
+
+    div.innerHTML = `<embed src="docs/CA1_descriptor.pdf" type="application/pdf" width="100%" height="600px" />`
+  }
 
 function submitForm(){
 
