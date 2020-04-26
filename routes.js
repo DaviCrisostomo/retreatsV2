913db2ce7d53264dccf7ed1f8da0b2f0f18ded7b
DaviCrisostomo <davicrisostomo@gmail.com>
2020-04-26 15:12:07 +0100

    routes, bloody routes...

--- routes.js
+++ routes.js
@@ -1,22 +1,12 @@
 var express = require('express'),
 router = express.Router(),
 retreatCtrl = require('./retreat_controller');
-
+//All routes used by the CRUD functions
+//I just copied them from your class, as you can see
 router.post('/retreat', retreatCtrl.createRetreat);
 router.get('/retreat', retreatCtrl.getRetreats);
 router.get('/retreat/:id', retreatCtrl.getRetreat);
 router.delete('/retreat/:id', retreatCtrl.deleteRetreat);
 router.put('/retreat/:id', retreatCtrl.updateRetreat);
-/*
-module.exports.UPLOAD_PATH = 'uploads';
 
-var multer = require('multer');
-var upload = multer({ dest: module.exports.UPLOAD_PATH });
-var imageCtrl = require('./image-controller');
-
-router.post('/images', upload.single('image'), imageCtrl.uploadImage);
-router.get('/images', imageCtrl.getImages);
-router.get('/images/:id', imageCtrl.getImage);
-router.delete('/images/:id', imageCtrl.deleteImage);
-*/
 module.exports = router;
