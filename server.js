b8940d1ca5e9e65763388f367be42adbfb861c67
DaviCrisostomo <davicrisostomo@gmail.com>
2020-04-26 21:28:23 +0100

    reverting heroku changes

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ mongoose = require('mongoose');
 require('dotenv').config()
 const morgan = require('morgan')
 var app = express();
-var port = process.env.PORT || 3000;//fuser -k 8080/tcp
+var port = 3000;//fuser -k 8080/tcp//process.env.PORT || 
 // Morgan
 app.use(morgan('tiny'))
 //route
