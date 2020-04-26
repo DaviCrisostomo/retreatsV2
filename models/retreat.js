e1ce2ecf53c5b22154422685c5d8db485b166ec3
DaviCrisostomo <davicrisostomo@gmail.com>
2020-04-26 14:36:26 +0100

    mongoose schema

---
+++ models/retreat.js
@@ -0,0 +1,21 @@
+var mongoose = require('mongoose');
+var retreatSchema = new mongoose.Schema({ 
+
+    //id: {type: mongoose.Schema.Types.ObjectId, index: true, unique:true, auto: true}, //root element
+    title: String,
+    date: Date,
+    duration: Number,
+    description: String,
+    contactMail: {type:String, lowercase:true},
+    imgUrl: String,
+    rooms: [
+    {
+    couple: Boolean,
+    beds: Number,
+    bookings: Number,
+    price: Number
+    },
+    ]
+        });
+
+module.exports = mongoose.model('Retreats', retreatSchema);
